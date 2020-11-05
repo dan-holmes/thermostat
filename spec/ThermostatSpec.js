ca25b0826ecda29cac91edda52189cfdfd19d258
Dan Holmes <dh@danielholmes.com>
2020-11-05 12:08:43 +0000

    Thermostat starts at 20 degrees

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,10 @@
 describe("Thermostat", function () {
+  var thermostat
 
+  beforeEach(function () {
+    thermostat = new Thermostat
+  })
+  it("starts with a temperature of 20 degrees", function () {
+    expect(thermostat.temperature).toEqual(20)
+  })
 })
