d27bc660aa4d23708de4ad8320c8936cf9044ab7
Dan Holmes <dh@danielholmes.com>
2020-11-05 13:46:11 +0000

    refactor

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,24 +1,45 @@
 class Thermostat {
+
   constructor() {
     this.temperature = 20
+    this.powerSavingMode = true
+    this.MINIMUM_TEMPERATURE = 10
+    this.MAXIMUM_TEMPERATURE = 25
   }
 
   up() {
-    if (this.temperature == 25) {
-      error = "The maximum temperature is 25 degrees when power saving mode is on."
-      throw new Error(error)
-    }
+    this._checkMaximumTemperature()
     this.temperature++
   }
 
   down() {
-    if (this.temperature == 10) {
-      throw new Error("The minimum temperature is 10 degrees.")
-    }
+    this._checkMinimumTemperature()
     this.temperature--
   }
 
   powerSavingOn() {
     this.powerSavingMode = true
+  }
+
+  _checkMaximumTemperature() {
+    if (this.temperature == this.MAXIMUM_TEMPERATURE) {
+      error = `The maximum temperature is ${this.MAXIMUM_TEMPERATURE} degrees when power saving mode is ${this._powerSavingText()}.`
+      throw new Error(error)
+    }
+  }
+
+  _checkMinimumTemperature() {
+    if (this.temperature == this.MINIMUM_TEMPERATURE) {
+      error = `The minimum temperature is ${this.MINIMUM_TEMPERATURE} degrees.`
+      throw new Error(error)
+    }
+  }
+
+  _powerSavingText() {
+    if this.powerSavingMode {
+      return 'on'
+    } else {
+      return 'off'
+    }
   }
 }
