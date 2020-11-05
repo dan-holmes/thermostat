f1252bbd294bbbc9b4431375d0ee5d18da0d9c6c
Dan Holmes <dh@danielholmes.com>
2020-11-05 12:11:26 +0000

    Thermostat up and down functions

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,4 +7,12 @@ describe("Thermostat", function () {
   it("starts with a temperature of 20 degrees", function () {
     expect(thermostat.temperature).toEqual(20)
   })
+  it("can increase the temperature with an up function", function () {
+    thermostat.up()
+    expect(thermostat.temperature).toEqual(21)
+  })
+  it("can decrease the temperature with a down function", function () {
+    thermostat.down()
+    expect(thermostat.temperature).toEqual(19)
+  })
 })
